4babf6212bb601ee8b37578722e67aaac329f7b8
PinkIsBack <mark.bukovich@gmail.com>
2021-11-12 15:34:06 +0200

    structure

---
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipes-list',
+  templateUrl: './recipes-list.component.html',
+  styleUrls: ['./recipes-list.component.css']
+})
+export class RecipesListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A test recipe','This is simply a test', 'https://static.timesofisrael.com/atlantajewishtimes/uploads/2021/03/Passover-Recipe_-Brisket_3-15-21-200x200.jpg')
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
